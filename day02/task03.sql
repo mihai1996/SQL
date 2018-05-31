e7f0fdf254eaeebbbf5178954426df0897054876
mihai1996 <mihailascu13@gmail.com>
2018-05-31 17:33:59 +0300

    ex5

--- day02/task03.sql
+++ day02/task03.sql
@@ -1,3 +1,4 @@
+--SELECT * FROM SalesLT.ProductCategory
 USE AdventureWorksLT
 	;WITH
 		ProductUnion AS(
@@ -22,7 +23,7 @@ USE AdventureWorksLT
 			WHERE Description LIKE '%@KeyWord%'
 			),
 		ORDERS AS(
-			SELECT SalesOrderDetailID, CustomerID
+			SELECT SalesOrderDetailID, CustomerID, ProductID
 			FROM SalesLT.SalesOrderHeader A LEFT JOIN SalesLT.SalesOrderDetail B ON A.SalesOrderID = B.SalesOrderID
 			),
 		Clients AS(
@@ -31,18 +32,36 @@ USE AdventureWorksLT
 			),
 		Regions AS(
 			SELECT SalesOrderDetailID, CountryRegion, StateProvince 
-			FROM Clients C LEFT JOIN SalesLT.Address A ON C.AddressID = A.AddressID
+			FROM Clients C JOIN SalesLT.Address A ON C.AddressID = A.AddressID
 			),
 		GrupCountry AS(
-			SELECT CountryRegion, StateProvince, CN = COUNT(1) 
+			SELECT CountryRegion, StateProvince, Comenzi = COUNT(1) 
 			FROM Regions
 			GROUP BY CountryRegion, StateProvince
+			),
+		ProdID AS(
+			SELECT Name, ProductNumber, Color, StandardCost, CustomerID
+			FROM ORDERS O LEFT JOIN SalesLT.Product P ON O.ProductID = P.ProductID
+			),
+		Location AS(
+			SELECT Name, ProductNumber, Color, StandardCost, AddressID
+			FROM ProdID P LEFT JOIN SalesLT.CustomerAddress C ON P.CustomerID = C.CustomerID
+			),
+		SetLocation AS(
+			SELECT DISTINCT Name, ProductNumber, CountryRegion, StateProvince  
+			FROM Location L LEFT JOIN SalesLT.Address A ON L.AddressID = A.AddressID
+			),
+		GroupProd AS(
+			SELECT CountryRegion, StateProvince, ProdUnic = COUNT(1)  
+			FROM SetLocation
+			GROUP BY CountryRegion, StateProvince
 			)
-			--ex4
-			SELECT * FROM GrupCountry 
-			--SELECT * FROM Regions
-			--SELECT * FROM Clients
-			--SELECT * FROM ORDERS
+			--ex5
+			SELECT * FROM GroupProd
+			--SELECT *FROM SetLocation
+			--SELECT *FROM Location
+			--SELECT  * FROM ProdID
+			 
 			--ex1
 			--SELECT DISTINCT * FROM ProductAll
 			--ORDER BY ProductCategoryID
@@ -50,3 +69,8 @@ USE AdventureWorksLT
 			--SELECT * FROM SearchProduct
 			--EX3
 			--SELECT *FROM SearchDescription
+			--ex4
+			--SELECT * FROM GrupCountry 
+			--SELECT * FROM Regions
+			--SELECT * FROM Clients
+			--SELECT * FROM ORDERS
